7d3198809a66a6096a4996c93b9a8d3aa27df472
Yejun Su <goofan.su@gmail.com>
2025-01-27 22:34:24 +0800

    bump hugo
    v0.141.0+extended+withdeploy

--- flake.nix
+++ flake.nix
@@ -3,11 +3,23 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
-      in {
-        devShells.default = with pkgs;
-          mkShell { packages = [ hugo tailwindcss foreman ]; };
-      });
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default =
+          with pkgs;
+          mkShell {
+            packages = [
+              hugo
+              tailwindcss
+              foreman
+            ];
+          };
+      }
+    );
 }
